757cf38ee8e8f77e4d8ff99a85ac0f213a62137f
Nengyi Jonathan Jiang <theNerdyMango@gmail.com>
2022-02-16 11:52:35 -0600

    .

--- login/login.js
+++ login/login.js
@@ -2,6 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const app = express();
+const fs = require('fs');
+
+const db = require('../db');
 
 app.use(session({
 	secret: 'secret',
@@ -12,37 +15,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', function(req, res){res.sendFile(path.join(__dirname + '/index.html'))});
+app.get('/', function(req, res){res.redirect('/login')});
+
+app.get('/login', function(req, res){res.sendFile(path.join(__dirname + '/index.html'))});
 
 app.post('/auth', function(req, res) {
 	let username = req.body.username;
 	let password = req.body.password;
 	if (username && password) {
         if (username == "JJ" && password == "264") {
-            // Authenticate the user
             req.session.loggedin = true;
             req.session.username = username;
             res.redirect('/home');
         } else {
-            res.send('Incorrect Username and/or Password!');
+            res.redirect('/login#invalidlogin');
         }			
         res.end();
 	} else {
-		res.send('Please enter Username and Password!');
+		res.redirect('/login#invalidlogin');
 		res.end();
 	}
 });
 
-app.get('/home', function(request, response) {
-	// If the user is loggedin
-	if (request.session.loggedin) {
-		// Output username
-		response.send('Welcome back, ' + request.session.username + '!');
+app.post('/auth-signup', function(req, res) {
+	let username = req.body.username;
+	let password = req.body.password;
+	if (username && password) {
+		req.session.loggedin = true;
+		req.session.username = username;
+		res.redirect('/home');
+        res.end();
 	} else {
-		// Not logged in
-		response.send('Please login to view this page!');
+		res.redirect('/login#invalidlogin');
+		res.end();
 	}
-	response.end();
+});
+
+app.get('/home', function(req, res) {
+	if (req.session.loggedin) res.send(`Hi ${req.session.username}`)//res.sendFile('C:/Users/K1334755/Desktop/Projects/Other/Games/homepage/index.html')
+	else{res.redirect('/login'); res.end()}
 });
 
 app.listen(3000);
