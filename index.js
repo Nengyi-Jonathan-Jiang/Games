93f47335a74970672fbaa0a9c93f90757034d64a
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2022-02-18 10:51:35 -0600

    .

--- index.js
+++ index.js
@@ -1,17 +1,101 @@
 const express = require('express');
+const session = require('express-session');
+const fs = require('fs');
+const db = require('./db');
+const socketio = require('socket.io');
+const path = require('path');
+
 /** @type {Express} */
 const app = require('express')();
 /** @type {http.Server} */
 const server = require('http').Server(app);
-const fs = require('fs');
-const socketio = require('socket.io');
 /** @type {socketio.Server} */
 const io = require('socket.io')(server);
 
+
+// #region login stuff
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/login/style.css', function(req,res){res.sendFile(__dirname + '/login/style.css')});
+app.get('/', function(req, res){res.redirect('/login')});
+app.get('/login', function(req, res){res.sendFile(path.join(__dirname + '/login/login.html'))});
+app.get('/signup', function(req, res){res.sendFile(path.join(__dirname + '/login/signup.html'))});
+
+function isValidLogin(username,password){
+	for(let i of db.data.accounts){
+		if(i.username == username && i.password == Buffer.from(password).toString('base64')){
+			return true;
+		}
+	}
+	return false;
+}
+function createAccount(username,password){
+	if(db.data.accounts.find(i=>i.username == username)){
+		return false;
+	}
+	db.data.accounts.push({
+		username: username,
+		password: Buffer.from(password).toString('base64'),
+	});
+	db.saveData();
+	return true;
+}
+function loginAndRedirectToHome(req,res,username){
+	req.session.loggedin = true;
+	req.session.username = username;
+	res.redirect('/home#'+JSON.stringify({
+		username: req.session.username
+	}));
+}
+
+app.post('/auth', function(req, res) {
+	let username = req.body.username;
+	let password = req.body.password;
+	if (username && password) {
+		if (isValidLogin(username, password)) {
+			loginAndRedirectToHome(req,res,username);
+		} else {
+			res.redirect('/login#invalidlogin');
+		}			
+		res.end();
+	} else {
+		res.redirect('/login#invalidlogin');
+		res.end();
+	}
+});
+
+app.post('/auth-signup', function(req, res) {
+	let username = req.body.username;
+	let password = req.body.password;
+	if (username && password && createAccount(username, password)) {
+		loginAndRedirectToHome(req,res,username);
+	} else {
+		res.redirect('/signup#invalidsignup');
+		res.end();
+	}
+});
+
 server.listen(3000);
 
+// #endregion
+
 app.use('/files', express.static(__dirname));
-app.all('/', function(req, res){res.sendFile(__dirname + '/homepage/index.html')});
+app.get('/homepage/style.css',function(req,res){res.sendFile(__dirname+'/homepage/style.css')});
+app.get('/home', function(req, res) {
+	if (req.session.loggedin){
+		res.sendFile((__dirname + '/homepage/index.html'));
+	}
+	else{
+		res.redirect('/login');
+		res.end()
+	}
+});
 
 let GAMES = fs.readdirSync("./games");
 
